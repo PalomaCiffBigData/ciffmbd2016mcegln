f94eef61db8117ca9b6dde12861220f2d0836a7b
mat4os <mat4os@gmx.es>
2016-06-26 20:40:35 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ciffmbd2016mcegln',
+      version='6.0.1',  # Development release
+      description='Autopredict libaries from mat4os',
+      url='https://github.com/mat4os/ciffmbd2016mcegln',
+      author='mat4os',
+      author_email='mat4os@gmx.es',
+      license='MIT',
+      packages=['ciffmbd2016mcegln'],
+      zip_safe=False)
